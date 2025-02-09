080878fb531b189e6ac26aff4bae05a7d05f4014
Tham Minh Vu <minhvu@Minh-Vu.local>
2025-02-09 10:48:14 +0700

    message

--- bin/dart_application_basic.dart
+++ bin/dart_application_basic.dart
@@ -1,5 +1,5 @@
 void main() {
   double cellcious = 20;
   var fahrenheit = (cellcious*1.8) + 32;
-  print ("${cellcious} độ C bằng ${fahrenheit} độ F");
+  print ("${cellcious} độ C bằng ${fahrenheit}  F");
 }
